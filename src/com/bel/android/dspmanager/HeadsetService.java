d6c1c4a6139e15bcc39464129db5654543679ccb
nadlabak <pavel@doshaska.net>
2011-05-18 02:08:43 +0200

    fix the detection of an active a2dp connection
    Change-Id: Id08cfe0fe67efb8f0e5c81e8d4737aa14cf43c35

--- src/com/bel/android/dspmanager/HeadsetService.java
+++ src/com/bel/android/dspmanager/HeadsetService.java
@@ -16,6 +16,7 @@ import android.media.audiofx.AudioEffect;
 import android.media.audiofx.BassBoost;
 import android.media.audiofx.Equalizer;
 import android.media.audiofx.Virtualizer;
+import android.media.AudioManager;
 import android.os.IBinder;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
@@ -45,6 +46,7 @@ public class HeadsetService extends Service {
 	
     public static final UUID EFFECT_TYPE_NULL = UUID.fromString("ec7178ec-e5e1-4432-a3f4-4657e6795210");
 
+    private AudioManager mAudioManager;
     private AudioEffect compression;
 	private Equalizer equalizer;
 	private Virtualizer virtualizer;
@@ -53,8 +55,6 @@ public class HeadsetService extends Service {
 	protected boolean useHeadphone;
 
 	protected boolean inCall;
-
-	protected boolean bluetoothAudio;
 
 	/**
 	 * Update audio parameters when preferences have been updated.
@@ -79,30 +79,6 @@ public class HeadsetService extends Service {
 		}
 	};
 
-	private final BroadcastReceiver bluetoothReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			/* Work out what device was connected. Must be something that handles audio. */
-			BluetoothDevice bd = (BluetoothDevice) intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-			BluetoothClass bdc = bd.getBluetoothClass();
-			if (bdc == null) {
-				Log.i(TAG, intent.getAction() + ": missing class information on device: " + bd);
-				return;
-			}
-			
-			Log.i(TAG, intent.getAction() + ": bluetooth class is " + bdc);
-			if (bdc.getMajorDeviceClass() != BluetoothClass.Device.Major.AUDIO_VIDEO) {
-				return;
-			}
-			
-			/* I don't actually know at this point if user will actually enable this device,
-			 * or whether it just remains "connected". Knowing this would be useful.
-			 */
-			bluetoothAudio = intent.getAction().equals(BluetoothDevice.ACTION_ACL_CONNECTED);
-			Log.i(TAG, "Bluetooth plugged: " + bluetoothAudio);
-		}
-	};
-
 	private final PhoneStateListener mPhoneListener = new PhoneStateListener() {
 		@Override
 		public void onCallStateChanged(int state, String incomingNumber) {
@@ -116,7 +92,6 @@ public class HeadsetService extends Service {
 				inCall = false;
 				break;
 			}
-
 			updateDsp();
 		}
 	};
@@ -150,13 +125,10 @@ public class HeadsetService extends Service {
 		TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 		tm.listen(mPhoneListener, PhoneStateListener.LISTEN_CALL_STATE);
 		
-        registerReceiver(headsetReceiver, new IntentFilter(Intent.ACTION_HEADSET_PLUG));
-        registerReceiver(preferenceUpdateReceiver, new IntentFilter("com.bel.android.dspmanager.UPDATE"));
-
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
-        filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-        registerReceiver(bluetoothReceiver, filter);
+                registerReceiver(headsetReceiver, new IntentFilter(Intent.ACTION_HEADSET_PLUG));
+                registerReceiver(preferenceUpdateReceiver, new IntentFilter("com.bel.android.dspmanager.UPDATE"));
+                Context context = getApplicationContext();
+                mAudioManager = (AudioManager)context.getSystemService(context.AUDIO_SERVICE);
 	}
 	
 	@Override
@@ -168,7 +140,6 @@ public class HeadsetService extends Service {
 		
 		unregisterReceiver(headsetReceiver);
 		unregisterReceiver(preferenceUpdateReceiver);
-		unregisterReceiver(bluetoothReceiver);
 		TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
 		tm.listen(mPhoneListener, 0);
 	}
@@ -187,7 +158,7 @@ public class HeadsetService extends Service {
 		if (inCall) {
 			/* During calls, everything gets disabled; there is no configuration called 'disable' */
 			mode = "disable";
-		} else if (bluetoothAudio) {
+		} else if (mAudioManager.isBluetoothA2dpOn()) {
 			/* Bluetooth takes precedence over everything else */
 			mode = "bluetooth";
 		} else {
