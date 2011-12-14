5ab9d75b3596518a64aaec340b3a9c3d7fe1f1db
Steve Kondik <shade@chemlab.org>
2011-12-14 01:14:59 -0500

    Fix linebreaks in makefile
    Change-Id: Ie7b04722f4b0535ce5ad4cdf43ea982873fa3647

--- Android.mk
+++ Android.mk
@@ -8,9 +8,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-	    android-support-v13 \
-#terminator
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
