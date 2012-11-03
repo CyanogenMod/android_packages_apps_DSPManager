4688bad15646a52f622dcd9475e920c8fa5129ef
Arnav Gupta <championswimmer@gmail.com>
2012-11-03 16:24:16 +0530

    DSPManager overrides compiling MusicFX
    Technically speaking this flag should be present.
    Although the current common.mk config in cm vendor does not contain MusicFX to be built,
     but still since DSPManager is an equalization app and from Jellybean onwards AOSP has own equalisation app MusicFX
    so we should override it in our make file
    
    Change-Id: Ide8eaa51ef84ef7997030511d332f7e8e7de8e1c
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- Android.mk
+++ Android.mk
@@ -15,6 +15,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := DSPManager
 LOCAL_CERTIFICATE := platform
 
+LOCAL_OVERRIDES_PACKAGES := MusicFX
+
 include $(BUILD_PACKAGE)
 
 # Use the folloing include to make our test apk.
