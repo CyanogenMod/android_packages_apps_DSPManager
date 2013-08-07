4506575371a948fae192951a607dcf579288015e
Danny Baumann <dannybaumann@web.de>
2013-08-07 17:05:17 +0200

    Don't sign DSPManager with the platform certificate.
    There's no real need to do so, and it prevents disabling it.
    
    Change-Id: Ie4797103cf97eec2f37ebe900da5981c33eddd5d

--- Android.mk
+++ Android.mk
@@ -13,7 +13,6 @@ LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := DSPManager
-LOCAL_CERTIFICATE := platform
 
 LOCAL_OVERRIDES_PACKAGES := MusicFX
 
